99fe714322ac917e067cd2969f2e58c2bf2a5e3d
donghyuk <donghyuk@rainist.com>
2021-01-14 21:05:19 +0900

    Add Player struct initializer

--- Sources/Player/Player.swift
+++ Sources/Player/Player.swift
@@ -2,6 +2,10 @@ import HandSign
 
 public struct Player {
     public let name: String
+
+    public init(name: String) {
+        self.name = name
+    }
     
     public func throwing() -> HandSign {
         return HandSign.allCases.randomElement()!
